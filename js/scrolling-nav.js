243431ca27f65f04aebd01384b00cd565905aaa0
unknown <jbaker@WS-JBAKER.Corp.WD>
2015-02-24 12:03:18 -0500

    Beta version of the Scrolling logo navbar - First push

---
+++ js/scrolling-nav.js
@@ -0,0 +1,35 @@
+    $(window).scroll(function () {
+    var navbarColor = '69,103,137'; //blue color
+    var smallLogoHeight = $('.sm-logo').height();
+    var bigLogoHeight = $('.bg-logo').height();
+    var navbarHeight = $('.navbar').height();
+    var smallLogoEndPos = 0;
+    var smallSpeed = smallLogoHeight / bigLogoHeight;
+    var ySmall = $(window).scrollTop() * smallSpeed;
+    var smallPadding = navbarHeight - ySmall;
+    if (smallPadding > navbarHeight) {
+        smallPadding = navbarHeight;
+    }
+    if (smallPadding < smallLogoEndPos) {
+        smallPadding = smallLogoEndPos;
+    }
+    if (smallPadding < 0) {
+        smallPadding = 0;
+    }
+    $('.sm-logo-container ').css({ 'padding-top': smallPadding });
+    var navOpacity = ySmall / smallLogoHeight;
+    if (navOpacity > 1) {
+        navOpacity = 1;
+    }
+    if (navOpacity < 0) {
+        navOpacity = 0;
+    }
+    var navBackColor = 'rgba(' + navbarColor + ',' + navOpacity + ')';
+    $('.navbar').css({ 'background-color': navBackColor });
+    var shadowOpacity = navOpacity * 0.6;
+    if (ySmall > 1) {
+        $('.navbar').css({ 'box-shadow': '0 2px 3px rgba(0,0,0,' + shadowOpacity + ')' });
+    } else {
+        $('.navbar').css({ 'box-shadow': 'none' });
+    }
+});
